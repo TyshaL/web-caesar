c341dac92a759c5c4972ec9a298382cecaa04443
TyshaL <91junebug@gmail.com>
2017-09-25 19:47:44 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <form = method = "POST">
+         <input = name = "rot" value ="0" type="text" />
+         < textarea type= "text" value ="0" name="text" />
+         <input type = "submit" />
+      </form>
+    </body>
+</html>
